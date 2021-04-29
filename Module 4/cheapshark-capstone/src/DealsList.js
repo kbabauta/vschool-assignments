9c8017f10d92658bede8a14901455c08dfcaaecb
Kevin Babauta <kjr.babauta@gmail.com>
2021-04-29 14:31:46 -0700

    updating capstone

--- Module 4/cheapshark-capstone/src/DealsList.js
+++ Module 4/cheapshark-capstone/src/DealsList.js
@@ -1,8 +1,16 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import axios from "axios"
 
 function DealsList() {
-    
+    const [deals, setDeals] = useState(null)
+
+    axios.get(`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15`)
+        .then (function (response) {
+            console.log(response)
+        })
+        .catch (function (error) {
+            console.log(error)
+        })
     return (
         <div>
             <h1>Deals List Page</h1>
--- Module 4/cheapshark-capstone/src/Header.js
+++ Module 4/cheapshark-capstone/src/Header.js
@@ -1,8 +1,8 @@
-import React, {Component} from "react"
+import React from "react"
 import {Link} from "react-router-dom"
 import "./styles.css"
 
-function Header(props) {
+function Header() {
     return (
         <div>
             <nav>
