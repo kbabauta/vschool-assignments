9068d88d3cf1709bd26ee05c0c74c73a3c47b6df
Kevin Babauta <kjr.babauta@gmail.com>
2021-04-29 13:52:13 -0700

    updating capstone project

--- Module 4/cheapshark-capstone/src/DealsList.js
+++ Module 4/cheapshark-capstone/src/DealsList.js
@@ -2,20 +2,10 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 
 function DealsList() {
-    const [topDeals, setDeals] = useState([])
-
-    useEffect(() => {
-        try {
-            axios.get(`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15 `).then(response => {
-                setDeals(response.data.dealRating)
-            })
-        } catch (error) { console.log(error) }
-    }, [])
-
+    
     return (
         <div>
             <h1>Deals List Page</h1>
-
         </div>
     )
 }
