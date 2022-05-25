4aaa7a3097797a68f2cf45f7b439278afb45279f
Kevin Babauta <kjr.babauta@gmail.com>
2022-05-25 15:43:26 -0700

    finished models on backend

--- Module 6/shutter-talk/client/src/App.jsx
+++ Module 6/shutter-talk/client/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Auth from './components/Auth'
+import Navbar from './components/Navbar'
+import Profile from './components/Profile'
+import Public from './components/Public'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+        <Auth/>
+        <Navbar/>
+        <Profile/>
+        <Public/>
+    </div>
   )
 }
 
