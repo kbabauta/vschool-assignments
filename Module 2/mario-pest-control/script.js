24c934dd98d17b636aa6cb9384843afec191ce0c
Kevin Babauta <kjr.babauta@gmail.com>
2022-06-10 10:35:31 -0700

    renaming folders

--- Module 2/mario-pest-control/script.js
+++ Module 2/mario-pest-control/script.js
@@ -1,7 +1,7 @@
 function getTotal() {
-    return (document.getElementById("goombaTotal").value * 5) +
-        (document.getElementById("bobombTotal").value * 7) +
-        (document.getElementById("cheepCheepTotal").value * 11);
+    return  (document.getElementById("goombaTotal").value * 5) +
+            (document.getElementById("bobombTotal").value * 7) +
+            (document.getElementById("cheepCheepTotal").value * 11);
 }
 
 document.getElementById("submit").addEventListener('click', function(e) {
